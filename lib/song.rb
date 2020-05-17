ce715719b0abe2d17776fa1c0e520bb2dcaf5333
Jessica Gamez <jpgamez1789@icloud.com>
2020-05-17 20:11:01 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -13,6 +13,9 @@ class Song
   
   def self.new_by_filename (filename)
     array = filename.split(" - ")
+    new_song = self.new(array[1])
+    new_song.artist_name = array[0]
+    new_song
   end
 
 
